c5b4e8cc0889229ae1904d5c64ad26bb1dfc0b21
James Turello and Maldon Meehan <james@>
2016-09-27 11:09:07 -0700

    add env file with api user/auth token

--- app/models/message.rb
+++ app/models/message.rb
@@ -7,17 +7,15 @@ private
     begin
       response = RestClient::Request.new(
         :method => :post,
-        :url => "https://api.twilio.com/2010-04-01/Accounts/AC11e0866641c6d3f3a96a7f9baa6e3ef9/Messages.json",
-        :user => 'AC11e0866641c6d3f3a96a7f9baa6e3ef9',
-        :password => 'd7ddbab262f597311e94e88a582fb7c1',
+        :url => "https://api.twilio.com/2010-04-01/Accounts/#{ENV['TWILIO_ACCOUNT_SID']}/Messages.json",
+        :user => ENV['TWILIO_ACCOUNT_SID'],
+        :password => ENV['TWILIO_AUTH_TOKEN'],
         :payload => { :Body => body,
                       :To => to,
                       :From => from }
       ).execute
-    rescue RestClient::BadRequest => error
-      message = JSON.parse(error.response)['message']
-      errors.add(:base, message)
-      false
+    rescue
+      throw(:abort)
     end
   end
 end
